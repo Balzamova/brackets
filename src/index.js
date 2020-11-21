a2672a898fc94dde0bbfd2139afc67099bd294ad
Balzamova <balzam0401@gmail.com>
2020-11-21 12:56:40 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [],
+    pairOfBrackets = {
+      '(': ')',
+      '{': '}',
+      '[': ']'
+    },
+    strToArr = str.split('');
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pairOfBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  
+  for (let i = 0; i < strToArr.length; i++) {
+    if (strToArr[i] === (stack[stack.length - 1])) {
+      stack.pop();
+    } else {
+      stack.push(pairOfBrackets[strToArr[i]]);
+    }
+  }
+
+  return stack.length === 0;
 }
